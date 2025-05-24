3469f58a2721c963ef93b89619cd033cdc006631
salimon victor <victorsalimon6@gmail.com>
2025-05-24 22:37:29 +0100

    second commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2">
-      <div className="container mx-auto flex h-16 items-center justify-around px-4">
+      <div className="container mx-auto flex h-16 items-center gap-5 justify-items-center md:justify-between px-4">
         <Link to={"/"}>
           <img
             src={theme === "dark" ? "/logo.png" : "/logo2.png"}
